8a30c31e780f3c051947cae45bed20af59b349c3
Wataru Shimizu <waruzilla@gmail.com>
2021-07-05 00:23:30 +0900

    Add opencv_contrib dir in settings-sample.sh

--- settings-sample.sh
+++ settings-sample.sh
@@ -1,8 +1,9 @@
 # Copy this file to samples.sh, then modify values according to
 # your environment
 opencv_src_dir="/path1/to1/opencv"
-opencv_build_dir="/path2/to2/build-opencv"
-opencv_install_dir="/path3/to3/opencv"
+opencv_contrib_src_dir="/path2/to2/opencv_contrib"
+opencv_build_dir="/path3/to3/build-opencv"
+opencv_install_dir="/path4/to4/opencv"
 
 # Settings for ropencv2 development
 bindtest_only=0
