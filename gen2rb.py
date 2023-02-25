f138235e98fe908d816d5e839c6f2960fa84bfb0
Wataru Shimizu <waruzilla@gmail.com>
2023-02-25 23:44:04 +0900

    Add RubyWrapperGenerator class for gen() method instead of global function
    as python-binding does

--- gen2rb.py
+++ gen2rb.py
@@ -410,146 +410,149 @@ class Namespace:
         for i, name in enumerate(self.funcs):
             print(f"{indent}funcs[{i}] {name}")
 
-def gen(headers:list[str], out_dir:str):
-    classes: dict[str, ClassInfo] = {}
-    namespaces: dict[str, Namespace] = {}
-    parser = hdr_parser.CppHeaderParser(generate_umat_decls=False, generate_gpumat_decls=False)
-    for hdr in headers:
-        decls = parser.parse(hdr)
-        hdr_fname = os.path.split(hdr)[1]
-        hdr_stem = os.path.splitext(hdr_fname)[0]
-        out_json_path = f"./autogen/tmp-{hdr_stem}.json"
-        with open(out_json_path, "w") as f:
-            json.dump(decls, f, indent=2)
-        with open("./autogen/rbopencv_include.hpp", "w") as f:
-            f.write(f'#include "{hdr}"\n')
-        for decl in decls:
-            # for i in range(len(decl)):
-            #     if i == 3:
-            #         for j in range(len(decl[i])):
-            #             print(f"  item[{j}] {decl[i][j]}")
-            #     else:
-            #         if decl[i] is None:
-            #             print(f"{i} is_None")
-            #         else:
-            #             print(f"{i} {decl[i]}")
-            name = decl[0]
-            if name.startswith("struct") or name.startswith("class"):
-                cols = name.split(" ", 1)
-                stype = cols[0] # "struct" or "class"
-                name = cols[1]
-                classinfo = ClassInfo(name, decl)
-                if classinfo.name in classes:
-                    print(f"Generator error: class {classinfo.name} (cname={classinfo.cname}) already exists")
-                    exit(1)
-                classes[classinfo.name] = classinfo
-            else:
-                namespace, classes_list, barename = split_decl_name(decl[0], parser.namespaces)
-                #print(f"namespace: {namespace}, classes_list: {classes_list}, barename: {barename}")
-                cname = "::".join(namespace+classes_list+[barename])
-                name = barename
-                classname = ''
-                bareclassname = ''
-                if classes_list:
-                    classname = normalize_class_name('.'.join(namespace+classes_list))
-                    bareclassname = classes_list[-1]
-                namespace_str = '.'.join(namespace)
-                isconstructor = name == bareclassname
-                is_static = False
-                isphantom = False
-                for i, m in enumerate(decl[2]):
-                    print(f"{i} {m}")
-                    if m == "/S":
-                        is_static = True
-                if isconstructor:
-                    name = "_".join(classes_list[:-1]+[name])
-                #print(f"  name: {name}, cname: {cname}, bareclassname: {bareclassname}, namespace_str: {namespace_str}, isconstructor: {isconstructor}")
-                if is_static:
+class RubyWrapperGenerator:
+    def gen(self, headers:list[str], out_dir:str):
+        classes: dict[str, ClassInfo] = {}
+        namespaces: dict[str, Namespace] = {}
+        parser = hdr_parser.CppHeaderParser(generate_umat_decls=False, generate_gpumat_decls=False)
+        for hdr in headers:
+            decls = parser.parse(hdr)
+            hdr_fname = os.path.split(hdr)[1]
+            hdr_stem = os.path.splitext(hdr_fname)[0]
+            out_json_path = f"./autogen/tmp-{hdr_stem}.json"
+            with open(out_json_path, "w") as f:
+                json.dump(decls, f, indent=2)
+            with open("./autogen/rbopencv_include.hpp", "w") as f:
+                f.write(f'#include "{hdr}"\n')
+            for decl in decls:
+                # for i in range(len(decl)):
+                #     if i == 3:
+                #         for j in range(len(decl[i])):
+                #             print(f"  item[{j}] {decl[i][j]}")
+                #     else:
+                #         if decl[i] is None:
+                #             print(f"{i} is_None")
+                #         else:
+                #             print(f"{i} {decl[i]}")
+                name = decl[0]
+                if name.startswith("struct") or name.startswith("class"):
+                    cols = name.split(" ", 1)
+                    stype = cols[0] # "struct" or "class"
+                    name = cols[1]
+                    classinfo = ClassInfo(name, decl)
+                    if classinfo.name in classes:
+                        print(f"Generator error: class {classinfo.name} (cname={classinfo.cname}) already exists")
+                        exit(1)
+                    classes[classinfo.name] = classinfo
+                elif name.startswith("enum "):
                     pass
                 else:
-                    if classname and not isconstructor:
-                        func_map = classes[classname].methods
+                    namespace, classes_list, barename = split_decl_name(decl[0], parser.namespaces)
+                    #print(f"namespace: {namespace}, classes_list: {classes_list}, barename: {barename}")
+                    cname = "::".join(namespace+classes_list+[barename])
+                    name = barename
+                    classname = ''
+                    bareclassname = ''
+                    if classes_list:
+                        classname = normalize_class_name('.'.join(namespace+classes_list))
+                        bareclassname = classes_list[-1]
+                    namespace_str = '.'.join(namespace)
+                    isconstructor = name == bareclassname
+                    is_static = False
+                    isphantom = False
+                    for i, m in enumerate(decl[2]):
+                        print(f"{i} {m}")
+                        if m == "/S":
+                            is_static = True
+                    if isconstructor:
+                        name = "_".join(classes_list[:-1]+[name])
+                    #print(f"  name: {name}, cname: {cname}, bareclassname: {bareclassname}, namespace_str: {namespace_str}, isconstructor: {isconstructor}")
+                    if is_static:
+                        pass
                     else:
-                        func_map = namespaces.setdefault(namespace_str, Namespace()).funcs
-                    func = func_map.setdefault(name, FuncInfo(classname, name, cname, isconstructor, namespace_str, is_static))
-                    func.add_variant(decl, isphantom)
-                if classname and isconstructor:
-                    classes[classname].constructor = func
-    #for i, class_name in enumerate(classes):
-    #    print(f"classes[{i}] {class_name}")
-    #    classes[class_name].dump(1)
-    classlist = list(classes.items())
-    classlist.sort()
-    classlist1 = [(classinfo.decl_idx, name, classinfo) for name, classinfo in classlist]
-    classlist1.sort()
+                        if classname and not isconstructor:
+                            func_map = classes[classname].methods
+                        else:
+                            func_map = namespaces.setdefault(namespace_str, Namespace()).funcs
+                        func = func_map.setdefault(name, FuncInfo(classname, name, cname, isconstructor, namespace_str, is_static))
+                        func.add_variant(decl, isphantom)
+                    if classname and isconstructor:
+                        classes[classname].constructor = func
+        #for i, class_name in enumerate(classes):
+        #    print(f"classes[{i}] {class_name}")
+        #    classes[class_name].dump(1)
+        classlist = list(classes.items())
+        classlist.sort()
+        classlist1 = [(classinfo.decl_idx, name, classinfo) for name, classinfo in classlist]
+        classlist1.sort()
 
-    # gen wrapclass
-    with open("./autogen/rbopencv_wrapclass.hpp", "w") as f:
-        for decl_idx, name, classinfo in classlist1:
-            cClass = f"c{name}" # cFoo
-            wrap_struct = f"struct Wrap_{name}" # struct WrapFoo
-            classtype = f"{name}_type"
-            f.write(f"static VALUE {cClass};\n")
-            f.write(f"{wrap_struct} {{\n")
-            f.write(f"    {name}* v;\n")
-            f.write(f"}};\n")
-            f.write(f"static void wrap_{name}_free({wrap_struct}* ptr){{\n")
-            f.write(f"    delete ptr->v;\n")
-            f.write(f"    ruby_xfree(ptr);\n")
-            f.write(f"}};\n")
-            f.write(f"static const rb_data_type_t {classtype} {{\n")
-            f.write(f"    \"{name}\",\n")
-            f.write(f"    {{NULL, reinterpret_cast<RUBY_DATA_FUNC>(wrap_{name}_free), NULL}},\n")
-            f.write(f"    NULL, NULL,\n")
-            f.write(f"    RUBY_TYPED_FREE_IMMEDIATELY\n")
-            f.write(f"}};")
-            f.write(f"static {name}* get_{name}(VALUE self){{\n")
-            f.write(f"    {wrap_struct}* ptr;\n")
-            f.write(f"    TypedData_Get_Struct(self, {wrap_struct}, &{name}_type, ptr);\n")
-            f.write(f"    return ptr->v;\n")
-            f.write(f"}}\n")
-            f.write(f"static VALUE wrap_{name}_alloc(VALUE klass){{\n")
-            f.write(f"    {wrap_struct}* ptr = nullptr;\n")
-            f.write(f"    VALUE ret = TypedData_Make_Struct(klass, {wrap_struct}, &{name}_type, ptr);\n")
-            f.write(f"    ptr->v = new {classinfo.cname}();\n")
-            f.write(f"    return ret;\n")
-            f.write(f"}}\n")
-            f.write(f"static VALUE wrap_{name}_init(VALUE self){{\n")
-            f.write(f"    return Qnil;\n")
-            f.write(f"}}\n")
-    # gen class registration
-    with open("./autogen/rbopencv_classregistration.hpp", "w") as f:
-        for decl_idx, name, classinfo in classlist1:
-            cClass = f"c{name}" # cFoo
-            wrap_struct = f" struct Wrap_{name}" # struct WrapFoo
-            classtype = f"{name}_type"
-            f.write(f"{{\n")
-            f.write(f"    {cClass} = rb_define_class_under(mCV2, \"{name}\", rb_cObject);\n")
-            f.write(f"    rb_define_alloc_func({cClass}, wrap_{name}_alloc);\n")
-            f.write(f"    rb_define_private_method({cClass}, \"initialize\", RUBY_METHOD_FUNC(wrap_{name}_init), 0);\n")
-            for name, func in classinfo.methods.items():
-                wrapper_name = func.get_wrapper_name()
-                f.write(f"    rb_define_method({cClass}, \"{func.name}\", RUBY_METHOD_FUNC({wrapper_name}), -1);\n")
-            f.write(f"}}\n")
-    # gen funcs
-    with open("./autogen/rbopencv_funcs.hpp", "w") as f:
-        funcs:list[FuncInfo] = []
-        for ns_name, ns in sorted(namespaces.items()):
-            #print(f"ns_name: {ns_name}")
-            #ns.dump(1)
-            if ns_name.split(".")[0] != "cv":
-                continue
-            for name, func in sorted(ns.funcs.items()):
-                if func.isconstructor:
+        # gen wrapclass
+        with open("./autogen/rbopencv_wrapclass.hpp", "w") as f:
+            for decl_idx, name, classinfo in classlist1:
+                cClass = f"c{name}" # cFoo
+                wrap_struct = f"struct Wrap_{name}" # struct WrapFoo
+                classtype = f"{name}_type"
+                f.write(f"static VALUE {cClass};\n")
+                f.write(f"{wrap_struct} {{\n")
+                f.write(f"    {name}* v;\n")
+                f.write(f"}};\n")
+                f.write(f"static void wrap_{name}_free({wrap_struct}* ptr){{\n")
+                f.write(f"    delete ptr->v;\n")
+                f.write(f"    ruby_xfree(ptr);\n")
+                f.write(f"}};\n")
+                f.write(f"static const rb_data_type_t {classtype} {{\n")
+                f.write(f"    \"{name}\",\n")
+                f.write(f"    {{NULL, reinterpret_cast<RUBY_DATA_FUNC>(wrap_{name}_free), NULL}},\n")
+                f.write(f"    NULL, NULL,\n")
+                f.write(f"    RUBY_TYPED_FREE_IMMEDIATELY\n")
+                f.write(f"}};")
+                f.write(f"static {name}* get_{name}(VALUE self){{\n")
+                f.write(f"    {wrap_struct}* ptr;\n")
+                f.write(f"    TypedData_Get_Struct(self, {wrap_struct}, &{name}_type, ptr);\n")
+                f.write(f"    return ptr->v;\n")
+                f.write(f"}}\n")
+                f.write(f"static VALUE wrap_{name}_alloc(VALUE klass){{\n")
+                f.write(f"    {wrap_struct}* ptr = nullptr;\n")
+                f.write(f"    VALUE ret = TypedData_Make_Struct(klass, {wrap_struct}, &{name}_type, ptr);\n")
+                f.write(f"    ptr->v = new {classinfo.cname}();\n")
+                f.write(f"    return ret;\n")
+                f.write(f"}}\n")
+                f.write(f"static VALUE wrap_{name}_init(VALUE self){{\n")
+                f.write(f"    return Qnil;\n")
+                f.write(f"}}\n")
+        # gen class registration
+        with open("./autogen/rbopencv_classregistration.hpp", "w") as f:
+            for decl_idx, name, classinfo in classlist1:
+                cClass = f"c{name}" # cFoo
+                wrap_struct = f" struct Wrap_{name}" # struct WrapFoo
+                classtype = f"{name}_type"
+                f.write(f"{{\n")
+                f.write(f"    {cClass} = rb_define_class_under(mCV2, \"{name}\", rb_cObject);\n")
+                f.write(f"    rb_define_alloc_func({cClass}, wrap_{name}_alloc);\n")
+                f.write(f"    rb_define_private_method({cClass}, \"initialize\", RUBY_METHOD_FUNC(wrap_{name}_init), 0);\n")
+                for name, func in classinfo.methods.items():
+                    wrapper_name = func.get_wrapper_name()
+                    f.write(f"    rb_define_method({cClass}, \"{func.name}\", RUBY_METHOD_FUNC({wrapper_name}), -1);\n")
+                f.write(f"}}\n")
+        # gen funcs
+        with open("./autogen/rbopencv_funcs.hpp", "w") as f:
+            funcs:list[FuncInfo] = []
+            for ns_name, ns in sorted(namespaces.items()):
+                #print(f"ns_name: {ns_name}")
+                #ns.dump(1)
+                if ns_name.split(".")[0] != "cv":
                     continue
-                funcs.append(func)
-        for decl_idx, name, classinfo in classlist1:
-            for name, func in sorted(classinfo.methods.items()):
-                if func.isconstructor:
-                    continue
-                funcs.append(func)
-        for func in funcs:
-            func.gen_code(f, classes)
+                for name, func in sorted(ns.funcs.items()):
+                    if func.isconstructor:
+                        continue
+                    funcs.append(func)
+            for decl_idx, name, classinfo in classlist1:
+                for name, func in sorted(classinfo.methods.items()):
+                    if func.isconstructor:
+                        continue
+                    funcs.append(func)
+            for func in funcs:
+                func.gen_code(f, classes)
 
 headers_txt = "./headers.txt"
 if len(sys.argv) == 2:
@@ -562,7 +565,8 @@ with open(headers_txt) as f:
             headers.append(line)
 dstdir = "./autogen"
 os.makedirs(dstdir, exist_ok=True)
-gen(headers, dstdir)
+generator = RubyWrapperGenerator()
+generator.gen(headers, dstdir)
 
 with open("./autogen/support-status.csv", "w") as f:
     for func in g_log_processed_funcs:
