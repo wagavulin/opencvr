f783e7bd6b6a72ed29ff95ae082d7c855d568748
Wataru Shimizu <waruzilla@gmail.com>
2023-02-27 00:03:10 +0900

    Update dummycv for testing of added features
    submodule, enum, etc.
    Point class
    Link opencv lib files

--- dummycv/dummycv.cpp
+++ dummycv/dummycv.cpp
@@ -1,8 +1,1 @@
 #include "dummycv.hpp"
-
-namespace cv {
-int bindTest1(int a){
-    PRINT_CXXFUNC();
-    return a + 10;
-}
-}
--- dummycv/dummycv.hpp
+++ dummycv/dummycv.hpp
@@ -1,6 +1,7 @@
 #ifndef DUMMYCV_HPP
 #define DUMMYCV_HPP
 
+#include <opencv2/core/types.hpp>
 #include <cstdio>
 
 #define PRINT_FUNC() fprintf(stderr, "[%s]\n", __func__)
@@ -36,12 +37,45 @@
 
 namespace cv {
 
-// global functions
-CV_EXPORTS_W int bindTest1(int a);
+// global functions for test arguments and retval
+CV_EXPORTS_W int bindTest1(int a) { return a+a; } // Simple function
+// CV_EXPORTS_W double bindTest1(int a, CV_IN_OUT Point& b, CV_OUT int* c, int d=10, RNG* rng=0, double e=1.2);
+CV_EXPORTS_W void bindTest2(int a) { int tmp = a + 10; if (tmp) {} } // retval: void
+CV_EXPORTS_W int bindTest3(int a) { return a + a; }
+CV_EXPORTS_W void bindTest4(int a, CV_IN_OUT Point& pt) { pt.x += a; pt.y -= a; }
+// CV_EXPORTS_W void bindTest5(int a, CV_IN_OUT Point& pt, CV_OUT int* x);
+// CV_EXPORTS_W bool bindTest6(int a, CV_IN_OUT Point& pt, CV_OUT int* x);
+
+// CV_EXPORTS_W double bindTest_overload(Point a, Point b, double c);
+// CV_EXPORTS_W double bindTest_overload(RotatedRect a);
+
+CV_EXPORTS_W void bindTest_Out_Point(int a, CV_OUT Point& pt) { pt.x=a+10; pt.y=a-10; }
+// CV_EXPORTS_W void bindTest_InOut_Mat(CV_IN_OUT Mat& a);
+// CV_EXPORTS_W void bindTest_InOut_bool(CV_IN_OUT bool& a);
+// CV_EXPORTS_W void bindTest_InOut_uchar(CV_IN_OUT uchar& a);
+// CV_EXPORTS_W void bindTest_InOut_int(CV_IN_OUT int& a);
+// CV_EXPORTS_W void bindTest_Out_intp(CV_OUT int* a);
+// CV_EXPORTS_W void bindTest_InOut_size_t(CV_IN_OUT size_t& a);
+// CV_EXPORTS_W void bindTest_InOut_float(CV_IN_OUT float& a);
+// CV_EXPORTS_W void bindTest_InOut_double(CV_IN_OUT double& a);
+// CV_EXPORTS_W void bindTest_InOut_Size(CV_IN_OUT Size& a);
+// CV_EXPORTS_W void bindTest_InOut_Size2f(CV_IN_OUT Size2f& a);
+// CV_EXPORTS_W void bindTest_InOut_Point(CV_IN_OUT Point& a);
+// CV_EXPORTS_W void bindTest_InOut_Point2f(CV_IN_OUT Point2f& a);
+// CV_EXPORTS_W void bindTest_InOut_RotatedRect(CV_IN_OUT RotatedRect& a);
+// CV_EXPORTS_W void bindTest_InOut_vector_Point(CV_IN_OUT std::vector<Point>& a);
+
+// enum
+enum MyEnum1 {
+    MYENUM1_UNCHANGED = -1,
+    MYENUM1_GRAYSACLE =  0,
+    MYENUM1_COLOR     =  1,
+    MYENUM1_IGNORE_ORIENTATION = 128,
+};
 
 class CV_EXPORTS_W Foo {
 public:
-    //CV_WRAP static int smethod1(int a) { return a + 10; }
+    CV_WRAP static int smethod1(int a) { return a + 10; }
     CV_WRAP Foo() { PRINT_CXXFUNC(); }
     CV_WRAP Foo(int value1) : m_value1(value1) { PRINT_CXXFUNC(); }
     ~Foo() { PRINT_CXXFUNC(); }
@@ -66,12 +100,37 @@ public:
 // public:
 //     CV_WRAP virtual void apply() = 0;
 // };
-// namespace Ns1 {
-// class CV_EXPORTS_W Bar {
-// public:
-//     CV_WRAP Bar() {}
-// };
-// }
-}
+namespace Ns1 {
+enum MyEnum2 { // old style enum
+    MYENUM2_VALUE_A = -1,
+    MYENUM2_VALUE_B =  0,
+    MYENUM2_VALUE_C = 10,
+};
+enum class MyEnum3 { // enum class
+    MYENUM3_VALUE_P = 100,
+    MYENUM3_VALUE_Q = 110,
+    MYENUM3_VALUE_R = 120,
+};
+class CV_EXPORTS_W Bar {
+public:
+    CV_WRAP Bar() {}
+    CV_WRAP int method1(int a) { return a+10; }
+};
+namespace Ns11 { // sub-sub-namespace
+CV_EXPORTS_W int bindTest_Ns11(int a) { return a + 11; } // global function in sub-sub-namespace
+enum MyEnum4 {
+    MYENUM4_VALUE_1 = 1000,
+    MYENUM4_VALUE_2 = 1100,
+};
+class CV_EXPORTS_W SubSubC1 {
+public:
+    CV_WRAP SubSubC1() {}
+    CV_WRAP SubSubC1(int v1) : m_value1(v1) {}
+    CV_WRAP int method1(int a) { return a + m_value1; }
+    int m_value1{111};
+};
+} // namespace Ns11
+} // namespace Ns1
+} // namespace cv
 
 #endif
