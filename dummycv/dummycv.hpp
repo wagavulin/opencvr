d73f6050861ba13767d65a4f99a5bea9bb9bdb2f
Wataru Shimizu <waruzilla@gmail.com>
2023-02-28 01:35:46 +0900

    Add test of in-out arg (#17)

--- dummycv/dummycv.hpp
+++ dummycv/dummycv.hpp
@@ -60,7 +60,7 @@ CV_EXPORTS_W void bindTest_Out_Point(int a, CV_OUT Point& pt) { pt.x=a+10; pt.y=
 // CV_EXPORTS_W void bindTest_InOut_double(CV_IN_OUT double& a);
 // CV_EXPORTS_W void bindTest_InOut_Size(CV_IN_OUT Size& a);
 // CV_EXPORTS_W void bindTest_InOut_Size2f(CV_IN_OUT Size2f& a);
-// CV_EXPORTS_W void bindTest_InOut_Point(CV_IN_OUT Point& a);
+CV_EXPORTS_W void bindTest_InOut_Point(CV_IN_OUT Point& a) { a.x+=10; a.y+=10; }
 // CV_EXPORTS_W void bindTest_InOut_Point2f(CV_IN_OUT Point2f& a);
 // CV_EXPORTS_W void bindTest_InOut_RotatedRect(CV_IN_OUT RotatedRect& a);
 // CV_EXPORTS_W void bindTest_InOut_vector_Point(CV_IN_OUT std::vector<Point>& a);
