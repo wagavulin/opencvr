d1aa19824868f8bb6fce03be52e7312fed3bc9bb
Wataru Shimizu <waruzilla@gmail.com>
2023-03-25 22:44:19 +0900

    Update sample for latest version

--- samples/tiny.rb
+++ samples/tiny.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 $:.unshift __dir__ + "/.."
-
+require 'numo/narray'
 require 'cv2'
 
-img = CV2::imread(__dir__ + "/input.jpg")
+# Create 3 channels color (BGR) image buffer (width: 600, height: 400)
+img = Numo::UInt8.zeros(400, 600, 3)
 # draw a circle of radius: 100 at position (200, 200) with blue color
 # (BGR: 255, 0, 0), thickness: 3 and antialiased
 CV2::circle(img, [200, 200], 50, [255, 0, 0], thickness: 3, lineType: CV2::LINE_AA)
-CV2::imwrite(__dir__ + "/out.jpg", img)
+# Save the image to out.jpg
+CV2::imwrite("out.jpg", img)
