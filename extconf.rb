0d871d0c187ef198cc1de303416e33b3b8a5d081
Wataru Shimizu <waruzilla@gmail.com>
2023-03-25 17:33:57 +0900

    Support building with OpenCV in non-standard directory.
    extconf.rb refers and generates properly with `PKG_CONFIG_PATH`

--- extconf.rb
+++ extconf.rb
@@ -9,8 +9,15 @@ $LOAD_PATH.each{|lp|
 }
 
 opencv4_include_dir = `pkg-config --cflags-only-I opencv4`.chomp
+opencv4_ldflags = `pkg-config --libs-only-L opencv4`.chomp # -L/path/to/opencv4/lib
 $CPPFLAGS = opencv4_include_dir + " -I./dummycv"
-$LDFLAGS = "-L./dummycv -Wl,-rpath,'$$ORIGIN/dummycv'"
+$LDFLAGS = opencv4_ldflags
+if !opencv4_ldflags.empty?
+  opencv4_libdir = opencv4_ldflags[2..-1] # remove "-L"
+  $LDFLAGS += " -Wl,-rpath,'#{opencv4_libdir}'"
+end
+$LDFLAGS += " -L./dummycv -Wl,-rpath,'$$ORIGIN/dummycv'"
+pp $LDFLAGS
 opencv4_libs = `pkg-config --libs-only-l opencv4`.chomp
 $libs = opencv4_libs + " -ldummycv"
 create_makefile('cv2')
