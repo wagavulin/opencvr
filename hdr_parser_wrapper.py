eca399e940c7efce82b6962b8f4edb03d46a0be3
Wataru Shimizu <waruzilla@gmail.com>
2023-07-16 14:13:03 +0900

    Many updates

--- hdr_parser_wrapper.py
+++ hdr_parser_wrapper.py
@@ -41,13 +41,17 @@ class CvEnum:
 
 @dataclasses.dataclass
 class CvKlass:
-    filename:str      # header filename (for debug)
+    filename:str            # header filename (for debug)
+    ns:"CvNamespace|None"   # namespace if it's defined directly under namespace, else None
+    klass:"CvKlass|None"    # class if it's defined inside other class, else None
     name:str
+    klasses:list["CvKlass"] # classes/structs defined in this class
     enums:list[CvEnum]
     funcs:list[CvFunc]
     str_parent_klass:str|None
     parent_klass:"CvKlass|None"
     child_klasses:list["CvKlass"]
+    no_bind:bool = False
 
 @dataclasses.dataclass
 class CvNamespace:
@@ -61,6 +65,7 @@ class CvApi:
     cvnamespaces:dict[str,CvNamespace]
     cvenums:dict[str,CvEnum]
     cvklasses:dict[str,CvKlass]
+    cvfuncs:dict[str,CvFunc]
 
 def _parse_parent_klass_str(str_parent_klasses:str, str_this_klass:str) -> str|None:
     parent_class_strs = str_parent_klasses.split(",")
@@ -78,25 +83,28 @@ def parse_headers(headers:list[str]):
     cvklasses:dict[str,CvKlass] = {}
     cvnamespaces:dict[str,CvNamespace] = {}
     cvenums:dict[str,CvEnum] = {}
+    cvfuncs:dict[str,CvFunc] = {}
     parser = hdr_parser.CppHeaderParser(generate_umat_decls=False, generate_gpumat_decls=False)
     for hdr in headers:
         decls = parser.parse(hdr)
         for decl in decls:
-            d00 = decl[0].split()[0]
+            # Remove unexpected whitespace in decl[0] of "cv.ClassName.operator ()"
+            decl0 = decl[0].replace("operator ()", "operator()")
+            decl0_strs = decl0.split()
+            if len(decl0_strs) >= 2:
+                if not decl0_strs[0] in ["class", "struct", "enum"]:
+                    print(f"[Error] unsupported decl type: {decl[0]} in {hdr}")
+                    exit(0)
+            d00 = decl0.split()[0]
             if d00 in ["class", "struct"]:
-                clsname = decl[0].split()[1]
-                cvklass = CvKlass(filename=hdr, name=clsname, enums=[], funcs=[], str_parent_klass=None, parent_klass=None, child_klasses=[])
+                clsname = decl0.split()[1]
+                cvklass = CvKlass(filename=hdr, ns=None, klass=None, name=clsname, klasses=[], enums=[], funcs=[],
+                    str_parent_klass=None, parent_klass=None, child_klasses=[])
                 cvklasses[clsname] = cvklass
                 cvklass.str_parent_klass = _parse_parent_klass_str(decl[1], clsname)
                 ns = ".".join(clsname.split(".")[0:-1])
-                if ns in cvklasses.keys():
-                    pass # do nothing
-                elif ns in cvnamespaces.keys():
-                    cvnamespaces[ns].klasses.append(cvklass)
-                else:
-                    cvnamespaces[ns] = CvNamespace(name=ns, klasses=[cvklass], enums=[], funcs=[])
             elif d00 in ["enum"]:
-                ss = decl[0].split()
+                ss = decl0.split()
                 enum_name = ""
                 isscoped = False
                 if len(ss) == 2:
@@ -104,23 +112,14 @@ def parse_headers(headers:list[str]):
                 elif len(ss) == 3:
                     enum_name = ss[2]
                     isscoped = True
+                if not enum_name.startswith("cv."):
+                    # Exclude enums which are not under cv namespace
+                    # e.g. CpuFeatures, cvflann.flann_algorithm_t, etc.
+                    continue
                 enum = CvEnum(filename=hdr, ns=None, klass=None, name=enum_name, isscoped=isscoped, values=[])
                 for value_info in decl[3]:
                     value = CvEnumerator(name=value_info[0].split()[1], value=value_info[1])
                     enum.values.append(value)
-                ns_or_klass:str = ".".join(enum_name.split(".")[0:-1])
-                if ns_or_klass in cvklasses.keys():
-                    klass = cvklasses[ns_or_klass]
-                    klass.enums.append(enum)
-                    enum.klass = klass
-                else:
-                    if ns_or_klass in cvnamespaces.keys():
-                        ns = cvnamespaces[ns_or_klass]
-                    else:
-                        ns = CvNamespace(name=ns_or_klass, klasses=[], enums=[enum], funcs=[])
-                        cvnamespaces[ns_or_klass] = ns
-                    ns.enums.append(enum)
-                    enum.ns = ns
                 cvenums[enum_name] = enum
             else: # func
                 wrap_as = None
@@ -140,7 +139,7 @@ def parse_headers(headers:list[str]):
                     elif func_attr == "/PV":
                         ispurevirtual = True
                     else:
-                        print(f"[Warning] {decl[0]} has unsupported func attribute: {func_attr}")
+                        print(f"[Warning] {decl0} has unsupported func attribute: {func_attr}")
 
                 args = []
                 for arg_tuple in decl[3]:
@@ -164,64 +163,97 @@ def parse_headers(headers:list[str]):
                         elif arg_attr == "/Ref":
                             pass # no need to handle lvalueref
                         else:
-                            print(f"[Warning] {decl[0]} has unsupported arg attribute: {arg_attr}")
+                            print(f"[Warning] {decl0} has unsupported arg attribute: {arg_attr}")
                     cvarg = CvArg(tp=tp, name=arg_tuple[1], defval=arg_tuple[2], inputarg=inputarg, outputarg=outputarg)
                     args.append(cvarg)
 
-                func = CvFunc(filename=hdr, ns=None, klass=None, name=decl[0].split(".")[-1], wrap_as=wrap_as, isconst=isconst,
+                func = CvFunc(filename=hdr, ns=None, klass=None, name=decl0, wrap_as=wrap_as, isconst=isconst,
                     isstatic=isstatic, isvirtual=isvirtual, ispurevirtual=ispurevirtual, args=args)
-                ns_or_klass:str = ".".join(decl[0].split(".")[0:-1])
-                if ns_or_klass in cvklasses.keys(): # ns_or_klass is a class name
-                    klass = cvklasses[ns_or_klass]
-                    klass.funcs.append(func)
-                    func.klass = klass
-                else: # ns_or_klass is a namespace name
-                    if ns_or_klass in cvnamespaces.keys():
-                        ns = cvnamespaces[ns_or_klass]
-                    else:
-                        ns = CvNamespace(name=ns_or_klass, klasses=[], enums=[], funcs=[func])
-                        cvnamespaces[ns_or_klass] = ns
-                    ns.funcs.append(func)
-                    func.ns = ns
+                cvfuncs[decl0] = func
 
-    for clsname, cvklass in cvklasses.items():
+    for nsname in parser.namespaces:
+        ns = CvNamespace(nsname, klasses=[], enums=[], funcs=[])
+        cvnamespaces[nsname] = ns
+
+    for _, cvklass in cvklasses.items():
         if cvklass.str_parent_klass:
+            if not cvklass.str_parent_klass in cvklasses.keys():
+                print(f"[Warning] {cvklass.name} has parent class ({cvklass.str_parent_klass}, "
+                    "but not defined. Bind without parent class")
+                continue
             pklass = cvklasses[cvklass.str_parent_klass]
             cvklass.parent_klass = pklass
             pklass.child_klasses.append(cvklass)
-    cvapi = CvApi(cvnamespaces=cvnamespaces, cvenums=cvenums, cvklasses=cvklasses)
-    return cvapi
 
-def _show_all_funcs(cvapi:CvApi):
-    all_funcs:list[tuple[str,str,CvFunc]] = []
-    for _, cvns in cvapi.cvnamespaces.items():
-        for func in cvns.funcs:
-            all_funcs.append(("F", cvns.name, func))
-        for cvklass in cvns.klasses:
-            for func in cvklass.funcs:
-                all_funcs.append(("M", cvklass.name, func))
-    for t in all_funcs:
-        print(f"{t[0]} {t[1]} {t[2].name}", end="")
-        if t[2].wrap_as:
-            print(f" -> {t[2].wrap_as}")
+    for _, cvenum in cvenums.items():
+        ns_or_klass = ".".join(cvenum.name.split(".")[0:-1])
+        if ns_or_klass in parser.namespaces:
+            #print(f"ENUM {cvenum.name:40s} in ns")
+            ns = cvnamespaces[ns_or_klass]
+            ns.enums.append(cvenum)
+            cvenum.ns = ns
+        elif ns_or_klass in cvklasses.keys():
+            #print(f"ENUM {cvenum.name:40s} in class")
+            klass = cvklasses[ns_or_klass]
+            klass.enums.append(cvenum)
+            cvenum.klass = klass
         else:
-            print()
-        for arg in t[2].args:
-            print(f"  {arg.name}  {arg.tp}")
+            # Special handling for cv.PCA.Flags, cv.SVD.Flags, etc.
+            # If an enum (.e.g cv.Foo.Bar.Enum1) is not included in neither namespaces nor klasses,
+            # it's assumed that cv.Foo.Bar is a class without CV_EXPORTS_W, and cv.Foo is a namespace.
+            #print(f"ENUM {cvenum.name:40s} nb_class")
+            nsname = ".".join(ns_or_klass.split(".")[0:-1])
+            if not nsname in cvnamespaces.keys():
+                print(f"[Error] {nsname} of {cvenum.name} is assumed to be a namespace, but not defined")
+                exit(1)
+            # Class name should start with small character or "_".
+            first_char = ns_or_klass.split(".")[-1][0]
+            if not (first_char.isupper() or first_char == "_"):
+                print(f"[Error] {ns_or_klass} of {cvenum} is probably a class, but does not start with [A-Z_]")
+                exit(1)
+            ns = cvnamespaces[nsname]
+            klass = CvKlass(filename=cvenum.filename, ns=ns, klass=None, name=ns_or_klass, klasses=[], enums=[], funcs=[],
+                str_parent_klass=None, parent_klass=None, child_klasses=[], no_bind=True)
+            cvklasses[ns_or_klass] = klass
+            klass.enums.append(cvenum)
+            cvenum.klass = klass
 
-def _show_all_enums(cvapi:CvApi):
-    for _, cvenum in cvapi.cvenums.items():
-        print(f"{cvenum.name} in {cvenum.filename}")
-        if cvenum.ns:
-            print(f"  NS {cvenum.ns.name}")
-        elif cvenum.klass:
-            print(f"   C {cvenum.klass.name}")
-        for value in cvenum.values:
-            print(f"    {value.name}  {value.value}")
+    sorted_klassnames = sorted(cvklasses.keys())
+    for klassname in sorted_klassnames:
+        cvklass = cvklasses[klassname]
+        ns_or_klass = ".".join(cvklass.name.split(".")[0:-1])
+        if ns_or_klass in cvnamespaces.keys():
+            ns = cvnamespaces[ns_or_klass]
+            ns.klasses.append(cvklass)
+            cvklass.ns = ns
+            #print(f"CLASS {cvklass.name:40s}  ns: {ns.name}")
+        elif ns_or_klass in cvklasses.keys():
+            defining_klass = cvklasses[ns_or_klass]
+            defining_klass.klasses.append(cvklass)
+            cvklass.klass = defining_klass
+            #print(f"CLASS {cvklass.name:40s}  class: {defining_klass.name}")
+        else:
+            print(f"[Error] class {cvklass.name} is not defined in neither namespaces nor other classes")
+            exit(0)
 
-def _show_all_namespaces(cvapi:CvApi):
-    for _, cvns in cvapi.cvnamespaces.items():
-        print(f"{cvns.name}")
+    for _, cvfunc in cvfuncs.items():
+        ns_or_klass = ".".join(cvfunc.name.split(".")[0:-1])
+        if ns_or_klass in cvnamespaces.keys():
+            ns = cvnamespaces[ns_or_klass]
+            ns.funcs.append(cvfunc)
+            cvfunc.ns = ns
+            #print(f"FUNC {cvfunc.name:40s}  ns: {ns.name}")
+        elif ns_or_klass in cvklasses.keys():
+            klass = cvklasses[ns_or_klass]
+            klass.funcs.append(cvfunc)
+            cvfunc.klass = klass
+            #print(f"FUNC {cvfunc.name:40s}  class: {klass.name}")
+        else:
+            print(f"[Error] FUNC {cvfunc.name} is not defined in neither namespaces nor other classes")
+            exit(0)
+
+    cvapi = CvApi(cvnamespaces=cvnamespaces, cvenums=cvenums, cvklasses=cvklasses, cvfuncs=cvfuncs)
+    return cvapi
 
 # headers_txt = "./headers.txt"
 # headers = []
